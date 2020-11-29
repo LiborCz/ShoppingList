d43189d1d7f1288d4263a2f5b101be44c007edab
Libor Pilný <libor.pilny@gmail.com>
2020-11-29 11:24:44 +0100

    Changes in deployment

--- server.js
+++ server.js
@@ -51,5 +51,5 @@ if (process.env.NODE_ENV === 'production') {
 }
 else {
   const port = process.env.PORT || 5000;
-  app.listen(port, () => console.log(`Server started on port ${port}`));
+  app.listen(port, '0.0.0.0', () => console.log(`Server started on port ${port}`));
 }
